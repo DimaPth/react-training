eac7fe930af943e6377e15501af424fdb15e11c1
DimaPth <mr.petuhdima@mail.ru>
2021-11-11 13:14:51 +0300

    Updates

--- src/store/action-creators/auth.js
+++
@@ -1,8 +0,0 @@
-import {SET_AUTH, SET_USER, SET_IS_LOADING, SET_IS_ERROR} from '../reducers/auth';
-
-export const setIsAuth = (payload) => ({ type: SET_AUTH, payload });
-export const setUser = (payload) => ({ type: SET_USER, payload });
-export const setIsLoading = (payload) => ({ type: SET_IS_LOADING, payload });
-export const setIsError = (payload) => ({ type: SET_IS_ERROR, payload });
-
-
--- src/store/action-creators/index.js
+++
@@ -1,9 +0,0 @@
-import * as MoviesActionCreators from "./movies";
-import * as MovieIDActionCreators from "./movieID";
-import * as AuthActionCreators from "./auth";
-
-export const ActionCreators = {
-	...MoviesActionCreators,
-	...MovieIDActionCreators,
-	...AuthActionCreators,
-};
